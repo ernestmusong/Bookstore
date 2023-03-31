256c3b9bb4cb0c6c8dc51943b747eb94d5c706fd
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-03-31 06:40:23 +0100

    remove Error page route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from 'components/Layout';
 import Home from 'components/Home';
 import Categories from './components/Categories';
-import Error from './components/Error';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="categories" element={<Categories />} />
-      <Route path="*" element={<Error />} />
     </Route>
   </Routes>
 );
